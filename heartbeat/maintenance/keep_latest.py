d04aee1edfa2f7f6515267a1e1cf7c852a43b59d
Colin Zhong <colin25346@gmail.com>
2020-03-02 19:03:28 -0500

    new update

--- heartbeat/maintenance/keep_latest.py
+++ heartbeat/maintenance/keep_latest.py
@@ -14,7 +14,6 @@ def keep_latest(s):
         df = pd.read_html(sp100_url,header=0)[2].rename(columns={"Symbol": "symbol","Name":"company"})
     elif dbname == 'nasdaq100':
         df = pd.read_html(nasdaq100_url,header=0)[2].rename(columns={"Ticker": "symbol","Company":"company"})
-        print(df)
     elif dbname == 'tsxci':
         df = pd.read_html(tsxci_url, header=0, keep_default_na=False)[1].rename(columns={"Symbol": "symbol","Company":"company"})
         df['symbol'] = df.replace({'symbol': r'\.'}, {'symbol': '-'}, regex=True)
